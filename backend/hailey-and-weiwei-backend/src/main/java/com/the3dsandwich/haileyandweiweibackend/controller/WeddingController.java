0f15097b0efe6fe4fd8f361b81ffc4412504b118
Shann Wei Yeh <weiwei.sw.yeh@gmail.com>
2024-02-10 00:56:10 +0800

    [dev] [backend] test CORS

--- backend/hailey-and-weiwei-backend/src/main/java/com/the3dsandwich/haileyandweiweibackend/controller/WeddingController.java
+++ backend/hailey-and-weiwei-backend/src/main/java/com/the3dsandwich/haileyandweiweibackend/controller/WeddingController.java
@@ -4,6 +4,7 @@ import com.the3dsandwich.haileyandweiweibackend.controller.bean.SignupWeddingRq;
 import com.the3dsandwich.haileyandweiweibackend.controller.bean.SignupWeddingRs;
 import com.the3dsandwich.haileyandweiweibackend.utils.HWStringUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class WeddingController {
 
     @PostMapping("/signup")
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     public SignupWeddingRs signupWedding(@RequestBody SignupWeddingRq request) {
         String responseMessage = HWStringUtils.format("I WILL CALL YOU, {}, BECAUSE I KNOW YOUR PHONE IS {} AND EMAIL {} AND YOU SAID \"{}\"", request.getName(), request.getPhone(), request.getEmail(), request.getComments());
         log.info("received request:\n{}", request.getEmail());
