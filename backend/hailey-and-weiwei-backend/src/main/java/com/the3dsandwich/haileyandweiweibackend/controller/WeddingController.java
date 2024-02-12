bd5a5a9688c84f08b3fe6e4b5558055dbdbde5bf
Shann Wei Yeh <weiwei.sw.yeh@gmail.com>
2024-02-12 20:51:50 +0800

    [dev] [backend] sample get endpoint for testing

--- backend/hailey-and-weiwei-backend/src/main/java/com/the3dsandwich/haileyandweiweibackend/controller/WeddingController.java
+++ backend/hailey-and-weiwei-backend/src/main/java/com/the3dsandwich/haileyandweiweibackend/controller/WeddingController.java
@@ -4,9 +4,13 @@ import com.the3dsandwich.haileyandweiweibackend.controller.bean.SignupWeddingRq;
 import com.the3dsandwich.haileyandweiweibackend.controller.bean.SignupWeddingRs;
 import com.the3dsandwich.haileyandweiweibackend.utils.HWStringUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /*
  * Copyright (c) 2024. the3dsandwich (Shann Wei Yeh)
@@ -22,6 +26,17 @@ public class WeddingController {
         return SignupWeddingRs.builder()
                               .message(responseMessage)
                               .build();
+    }
+
+    @GetMapping("testList")
+    public List<SignupWeddingRs> testList() {
+        List<SignupWeddingRs> response = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            response.add(SignupWeddingRs.builder()
+                                        .message(HWStringUtils.format("{}", i))
+                                        .build());
+        }
+        return response;
     }
 
 }
