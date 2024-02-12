2a5c019b84bb4bb6bf93e06545748564329de333
Shann Wei Yeh <weiwei.sw.yeh@gmail.com>
2024-02-12 21:18:59 +0800

    [dev] [backend] logging for incoming/outgoing requests and adjust log level

--- backend/hailey-and-weiwei-backend/src/main/java/com/the3dsandwich/haileyandweiweibackend/HaileyAndWeiweiBackendApplication.java
+++ backend/hailey-and-weiwei-backend/src/main/java/com/the3dsandwich/haileyandweiweibackend/HaileyAndWeiweiBackendApplication.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -33,6 +34,16 @@ public class HaileyAndWeiweiBackendApplication {
                         .allowedOrigins(allowedOrigin);
             }
         };
+    }
+
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeClientInfo(true);
+        loggingFilter.setIncludeQueryString(true);
+        loggingFilter.setIncludePayload(true);
+        loggingFilter.setMaxPayloadLength(64000);
+        return loggingFilter;
     }
 
 }
--- backend/hailey-and-weiwei-backend/src/main/java/com/the3dsandwich/haileyandweiweibackend/controller/WeddingController.java
+++ backend/hailey-and-weiwei-backend/src/main/java/com/the3dsandwich/haileyandweiweibackend/controller/WeddingController.java
@@ -22,7 +22,6 @@ public class WeddingController {
     @PostMapping("/signup")
     public SignupWeddingRs signupWedding(@RequestBody SignupWeddingRq request) {
         String responseMessage = HWStringUtils.format("You, {}, will be called, BECAUSE I KNOW YOUR PHONE IS {} AND EMAIL {} AND YOU SAID \"{}\"", request.getName(), request.getPhone(), request.getEmail(), request.getComments());
-        log.info("received request:\n{}", request.getEmail());
         return SignupWeddingRs.builder()
                               .message(responseMessage)
                               .build();
