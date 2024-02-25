e7f48b4f28f0cfdb2e39a9b4265d3c3aa654b6e8
Shann Wei Yeh <weiwei.sw.yeh@gmail.com>
2024-02-25 14:07:24 +0800

    [dev] [backend] pretty print stuff

---
+++ backend/hailey-and-weiwei-backend/src/main/java/com/the3dsandwich/haileyandweiweibackend/utils/HWJsonUtils.java
@@ -0,0 +1,30 @@
+package com.the3dsandwich.haileyandweiweibackend.utils;/*
+ * Copyright (c) 2024. the3dsandwich (Shann Wei Yeh)
+ */
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import lombok.extern.slf4j.Slf4j;
+
+/*
+ * Copyright (c) 2024. the3dsandwich (Shann Wei Yeh)
+ */
+@Slf4j
+public class HWJsonUtils {
+
+    public static String toPrettyJson(Object target) {
+
+        try {
+            ObjectWriter ow = new ObjectMapper().writer()
+                                                .withDefaultPrettyPrinter();
+            return ow.writeValueAsString(target);
+        } catch (JsonProcessingException exception) {
+            log.error("json pretty print error: {}", exception.getMessage());
+            exception.printStackTrace();
+        }
+
+        return HWStringUtils.EMPTY;
+    }
+
+}
