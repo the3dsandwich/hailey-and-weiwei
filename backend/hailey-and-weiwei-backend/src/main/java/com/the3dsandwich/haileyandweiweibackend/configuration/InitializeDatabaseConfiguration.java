ee1ce0cdc037c74f8b6cb45de7d09fd359b755e3
Shann Wei Yeh <weiwei.sw.yeh@gmail.com>
2024-04-14 14:58:19 +0800

    [dev] [backend] initialize database

---
+++ backend/hailey-and-weiwei-backend/src/main/java/com/the3dsandwich/haileyandweiweibackend/configuration/InitializeDatabaseConfiguration.java
@@ -0,0 +1,40 @@
+package com.the3dsandwich.haileyandweiweibackend.configuration;
+
+import com.the3dsandwich.haileyandweiweibackend.service.data.GuestRepository;
+import com.the3dsandwich.haileyandweiweibackend.service.data.entity.GuestEntity;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/*
+ * Copyright (c) 2024. the3dsandwich (Shann Wei Yeh)
+ */
+@Slf4j
+@Configuration
+public class InitializeDatabaseConfiguration {
+
+    @Bean
+    CommandLineRunner initDatabase(GuestRepository guestRepository) {
+
+        return args -> {
+            // initialize database entries
+            guestRepository.save(GuestEntity.builder()
+                                            .name("Bride")
+                                            .email("bride@gmail.com")
+                                            .phone("0987654321")
+                                            .comments("新娘唷")
+                                            .tags("bride")
+                                            .build());
+            guestRepository.save(GuestEntity.builder()
+                                            .name("Groom")
+                                            .email("groom@gmail.com")
+                                            .phone("0912345678")
+                                            .comments("新郎唷")
+                                            .tags("groom")
+                                            .build());
+
+        };
+    }
+
+}
