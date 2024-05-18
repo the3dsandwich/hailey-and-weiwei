391bd0395e92c4ff772da684c9b11d46af87a028
Shann Wei Yeh <weiwei.sw.yeh@gmail.com>
2024-05-18 15:59:18 +0800

    [frontend] adjust heading with link

--- frontend/components/TopNav.tsx
+++ frontend/components/TopNav.tsx
@@ -10,13 +10,20 @@ const TopNav = () => {
   return (
     <nav className="flex flex-col absolute w-full h-56 items-center justify-center">
       <Link href={routeHome}>
-        <h1 className="text-red-100 font-extralight text-3xl">{title}</h1>
+        <h1 className="text-red-100 font-extralight text-3xl">
+          Hailey & Wei-Wei&apos;s
+        </h1>
       </Link>
       <p className="text-red-100 text-center mt-2">
         Wedding invitation
         <br />
         <small>
-          <i>1 Dec 2024 | Le Méridien Taipei</i>
+          <i>
+            1 Dec 2024 |{" "}
+            <a href="https://maps.app.goo.gl/cHEWEB56d5x1qjXv6" target="_blank">
+              Le Méridien Taipei
+            </a>
+          </i>
         </small>
         <br />
         <SignupForm small />
