61ea76c1d5da31edd00fd6552605edf4210d5b9d
Shann Wei Yeh <weiwei.sw.yeh@gmail.com>
2024-06-30 21:56:34 +0800

    [frontend] hearts for hr

--- frontend/app/component/Hr.tsx
+++ frontend/app/component/Hr.tsx
@@ -5,7 +5,7 @@ const Hr = () => {
     <div className="inline-flex items-center justify-center w-full">
       <hr className="w-64 h-px my-8 bg-red-200 border-0 dark:bg-red-700" />
       <span className="absolute px-3 text-red-900 -translate-x-1/2 bg-red-900 left-1/2 dark:text-red-100 dark:bg-red-900">
-        💎
+        🤍
       </span>
     </div>
   );
